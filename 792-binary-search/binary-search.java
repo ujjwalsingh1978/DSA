08517a500eef6a61248b38c94789d419b90e54ed
ujjwalsingh1978 <aidsews2303@glbitm.ac.in>
2026-09-11 21:14:34 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (91.30%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n  = nums.length; 
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]>target){
+                high = mid-1;
+
+            } 
+            else if(nums[mid]<target){
+                low = mid+1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+
+    }
+}
